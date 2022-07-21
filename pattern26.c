696ddde9f09aafa480585ab196e896d36d440acb
bannysha157 <98574529+bannysha157@users.noreply.github.com>
2022-07-21 11:39:34 +0530

    Create pattern26.c

---
+++ pattern26.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,rows=5;
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			(i%2!=0)?printf(" %c",i+96):printf(" %d",i);
+		}
+		printf("\n");
+	}
+	return 0;
+}
